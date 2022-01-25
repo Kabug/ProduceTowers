9423e05fc1df43bfbe339b74d8b341e2a7166b87
AndyZhou1998 <haonanzhou1998@gmail.com>
2022-01-24 21:20:29 -0800

    Added generate trees :wq

---
+++ Assets/Scripts/Generators/NodeTypes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeTypes
+{
+    GROUND,
+    ROCK,
+    WATER,
+    PATH,
+    PATH_TUNNEL,
+    PATH_BRIDGE,
+    START,
+    END
+}
